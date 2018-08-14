d25d5b8af5461078bb12e1a4efcee79b4eacfd9d
Trevor Atlas <trevoristall@gmail.com>
2018-08-13 22:28:50 -0400

    mock HTTP interface

---
+++ tests/rest_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"github.com/trevor-atlas/vor/rest"
+	"io"
+)
+
+type mockHTTP struct{}
+
+func (h *mockHTTP) Body(body io.Reader) rest.RequestBuilder {
+	return h
+}
+
+func (h *mockHTTP) WithHeader(key, value string) rest.RequestBuilder {
+	return h
+}
+
+func (h *mockHTTP) GET() ([]byte, error) {
+	return []byte{0, 0, 0, 0, 0}, nil
+}
+
+func (h *mockHTTP) POST() ([]byte, error) {
+	return []byte{0, 0, 0, 0, 0}, nil
+}
+
+func (h *mockHTTP) Url(url string) rest.RequestBuilder {
+	return h
+}
